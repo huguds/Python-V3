e3f53753d4769e13d8f8a3593b4e953e7a3753c1
huguds <vfvitinhoblack@gmail.com>
2022-01-23 10:43:43 -0300

    Mostrar a função print

---
+++ aula001-funcao-print.py
@@ -0,0 +1,14 @@
+# Programador: Victor Hugo
+# Data: 18/01/2022
+# Algoritmo: Print | Revisão
+
+"""
+Print - É responsável por imprimir os argumentos passados
+"""
+
+print(1) # Inteiro
+print('1') # String
+print("\n") # Pular linha
+print('This is my favorite book number: ', 8)
+print("This is my number:",8,"favorite book ")
+print(f'This is my favorite book number: {8}') # Utiizando o f é possivel imprimir valores nas aspas
