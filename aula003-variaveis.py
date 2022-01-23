2952cc90db5569d4da086eafee5e146ea2f1adb6
huguds <vfvitinhoblack@gmail.com>
2022-01-23 10:48:16 -0300

    variaveis

---
+++ aula003-variaveis.py
@@ -0,0 +1,23 @@
+# Programador: Victor Hugo
+# Data: 18/01/2022
+# Algoritmo: variaveis | Revisão
+
+"""
+Variaveis servem para armazenar valores que serão utilizados
+"""
+
+# inteiro
+x = 5
+y = 10
+z = 15
+# Float
+x = 1.5
+y = 2.01
+z = 1/3
+print(x,y,z,"\n") # imprime na tela
+
+x = y = z # Sempre será atribuido o ultimo valor da variavel que será z = 15
+print(x,"\n")
+
+x,y,z = 1,2,3 # É possivel criar mais de uma variavel e atribuir o valor na mesma linha
+print(x,y,z)
